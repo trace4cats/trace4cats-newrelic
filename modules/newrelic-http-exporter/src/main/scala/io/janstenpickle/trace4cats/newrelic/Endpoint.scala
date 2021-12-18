ec0be62a75ce0e10564ce887a3d8dd679536fd69
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-12-18 19:52:48 +0300

    feat: remove BlazeClient from dependencies
    Ref.: trace4cats/trace4cats-newrelic#55

--- modules/newrelic-http-exporter/src/main/scala/io/janstenpickle/trace4cats/newrelic/Endpoint.scala
+++ modules/newrelic-http-exporter/src/main/scala/io/janstenpickle/trace4cats/newrelic/Endpoint.scala
@@ -1,16 +1,24 @@
 package io.janstenpickle.trace4cats.newrelic
 
+import org.http4s.Uri
+
 sealed trait Endpoint {
-  def url: String
+  def uri: Uri
 }
 object Endpoint {
-  private def standardUrl(subdomain: String) = s"https://$subdomain.newrelic.com/trace/v1"
+  private def standardUri(subdomain: String) = Uri.unsafeFromString(s"https://$subdomain.newrelic.com/trace/v1")
 
   case object US extends Endpoint {
-    override val url: String = standardUrl("trace-api")
+    override val uri: Uri = standardUri("trace-api")
   }
+
   case object EU extends Endpoint {
-    override val url: String = standardUrl("trace-api.eu")
+    override val uri: Uri = standardUri("trace-api.eu")
   }
-  case class Observer(url: String) extends Endpoint
+
+  case class Observer(uri: Uri) extends Endpoint
+  object Observer {
+    @deprecated("Use Observer(uri: Uri) constructor", since = "0.13.0")
+    def apply(url: String): Observer = apply(Uri.unsafeFromString(url))
+  }
 }
